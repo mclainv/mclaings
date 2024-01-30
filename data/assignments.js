96bb8eb20729881de5e95dae114c2595fa7fad7c
planit <planit@tanjeffreyz.github.io>
2024-01-30 19:04:38 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,38 +1,24 @@
 assignments = {
-  "EECS 127/227AT-LEC-001": [
+  "SP24_MATH-UA_120_1_002": [
     {
-      "title": "Homework 01 PDF",
-      "course": "EECS 127/227AT-LEC-001",
-      "dueDate": "2024-01-19T23:00:00",
-      "link": "https://www.gradescope.com/courses/699270",
-      "submitted": false
-    },
-    {
-      "title": "Homework 01 Self Grade",
-      "course": "EECS 127/227AT-LEC-001",
-      "dueDate": "2024-01-26T23:00:00",
-      "link": "https://www.gradescope.com/courses/699270",
-      "submitted": false
-    },
-    {
-      "title": "Homework 02 PDF",
-      "course": "EECS 127/227AT-LEC-001",
-      "dueDate": "2024-01-26T23:00:00",
-      "link": "https://www.gradescope.com/courses/699270",
-      "submitted": false
-    },
-    {
-      "title": "Homework 03 PDF",
-      "course": "EECS 127/227AT-LEC-001",
+      "title": "Problem Set 1",
+      "course": "SP24_MATH-UA_120_1_002",
       "dueDate": "2024-02-02T23:00:00",
-      "link": "https://www.gradescope.com/courses/699270",
+      "link": "https://www.gradescope.com/courses/719878",
       "submitted": false
     },
     {
-      "title": "Homework 02 Self Grade",
-      "course": "EECS 127/227AT-LEC-001",
-      "dueDate": "2024-02-02T23:00:00",
-      "link": "https://www.gradescope.com/courses/699270/assignments/4010470/submissions/new",
+      "title": "Quiz 1 - Sections 1-3",
+      "course": "SP24_MATH-UA_120_1_002",
+      "dueDate": "2024-01-26T23:59:00",
+      "link": "https://www.gradescope.com/courses/719878",
+      "submitted": false
+    },
+    {
+      "title": "Quiz 2 - Sections 4-7",
+      "course": "SP24_MATH-UA_120_1_002",
+      "dueDate": "2024-09-19T23:00:00",
+      "link": "https://www.gradescope.com/courses/719878",
       "submitted": false
     }
   ]

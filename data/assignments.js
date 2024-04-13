f7ce3806410a4023138fb4ec95148ca100fdcf06
planit <planit@tanjeffreyz.github.io>
2024-04-13 04:35:03 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -146,6 +146,13 @@ assignments = {
       "dueDate": "2024-04-12T23:59:00",
       "link": "https://www.gradescope.com/courses/719878/assignments/4349058/submissions/246264310",
       "submitted": true
+    },
+    {
+      "title": "Problem Set 7",
+      "course": "SP24_MATH-UA_120_1_002",
+      "dueDate": "2024-04-19T23:00:00",
+      "link": "https://www.gradescope.com/courses/719878",
+      "submitted": false
     }
   ]
 };

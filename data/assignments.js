d82e25f95e9ad31f1ea2904164407646cb153d9c
planit <planit@tanjeffreyz.github.io>
2024-04-18 12:45:43 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -153,6 +153,13 @@ assignments = {
       "dueDate": "2024-04-19T23:00:00",
       "link": "https://www.gradescope.com/courses/719878",
       "submitted": false
+    },
+    {
+      "title": "Quiz 12 - Sections 35, 36",
+      "course": "SP24_MATH-UA_120_1_002",
+      "dueDate": "2024-04-19T23:59:00",
+      "link": "https://www.gradescope.com/courses/719878/assignments/4377324/submissions/new",
+      "submitted": false
     }
   ]
 };
